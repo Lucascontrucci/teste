cc154092d51adbae86084a13d9263a39e58eba5a
Adurra <giovanniultramari@gmail.com>
2023-10-27 22:30:54 -0300

    Project Init Spring/Gradle

---
+++ src/main/java/com/salesunity/appsu/AppsuApplication.java
@@ -0,0 +1,13 @@
+package com.salesunity.appsu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppsuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppsuApplication.class, args);
+	}
+
+}
